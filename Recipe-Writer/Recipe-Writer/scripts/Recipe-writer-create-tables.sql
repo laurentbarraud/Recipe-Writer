bbe07ad5781a0992e17c434c7b913ffd46ed1193
laurentbarraud <lau.barraud@gmail.com>
2025-04-07 20:51:16 +0200

    Version 1.1 - toutes fonctionnalités implémentées. Requêtes SQL optimisées et sécurisées. Entièrement localisée.

--- Recipe-Writer/Recipe-Writer/scripts/Recipe-writer-create-tables.sql
+++ Recipe-Writer/Recipe-Writer/scripts/Recipe-writer-create-tables.sql
@@ -1,22 +1,52 @@
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS "Ingredients";
-CREATE TABLE IF NOT EXISTS "Ingredients" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"ingredientName"	TEXT NOT NULL,
+CREATE TABLE "Ingredients" (
+	"id"	INTEGER NOT NULL,
+	"ingredientName_en"	TEXT,
+	"ingredientName_fr"	TEXT NOT NULL,
 	"qtyAvailable"	REAL NOT NULL,
 	"scale_id"	INTEGER NOT NULL,
 	"typeOfIngredient_id"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT),
 	FOREIGN KEY("scale_id") REFERENCES "Scales"("id"),
 	FOREIGN KEY("typeOfIngredient_id") REFERENCES "TypesOfIngredient"("id")
 );
-DROP TABLE IF EXISTS "TypesOfIngredient";
-CREATE TABLE IF NOT EXISTS "TypesOfIngredient" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"type"	TEXT
+DROP TABLE IF EXISTS "Instructions";
+CREATE TABLE "Instructions" (
+	"id"	INTEGER NOT NULL,
+	"instruction"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "Instructions_has_Recipes";
+CREATE TABLE "Instructions_has_Recipes" (
+	"id"	INTEGER NOT NULL,
+	"Recipes_id"	INTEGER NOT NULL,
+	"Instructions_id"	INTEGER NOT NULL,
+	"InstructionNb"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("Instructions_id") REFERENCES "Instructions" ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("Recipes_id") REFERENCES "Recipes" ON UPDATE CASCADE ON DELETE CASCADE
+);
+DROP TABLE IF EXISTS "PlannedMeals";
+CREATE TABLE "PlannedMeals" (
+	"id"	INTEGER NOT NULL,
+	"titleOfPlannedRecipe"	TEXT,
+	"dayOfTheWeek"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "Recipes";
+CREATE TABLE "Recipes" (
+	"id"	INTEGER NOT NULL,
+	"title"	TEXT NOT NULL,
+	"completionTime"	INTEGER NOT NULL,
+	"lowBudget"	INTEGER NOT NULL,
+	"score"	INTEGER NOT NULL,
+	"imagePath"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Recipes_has_Ingredients";
-CREATE TABLE IF NOT EXISTS "Recipes_has_Ingredients" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE "Recipes_has_Ingredients" (
+	"id"	INTEGER NOT NULL,
 	"qtyIngredient1"	REAL,
 	"qtyIngredient2"	REAL,
 	"qtyIngredient3"	REAL,
@@ -77,18 +107,8 @@ CREATE TABLE IF NOT EXISTS "Recipes_has_Ingredients" (
 	"scales18_id"	INTEGER,
 	"scales19_id"	INTEGER,
 	"scales20_id"	INTEGER,
-	FOREIGN KEY("ingredient8_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient6_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient1_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient2_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient3_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient4_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient5_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient7_id") REFERENCES "Ingredients",
-	FOREIGN KEY("ingredient9_id") REFERENCES "Ingredients",
-	FOREIGN KEY("scales10_id") REFERENCES "Scales",
+	PRIMARY KEY("id" AUTOINCREMENT),
 	FOREIGN KEY("ingredient10_id") REFERENCES "Ingredients",
-	FOREIGN KEY("scales9_id") REFERENCES "Scales",
 	FOREIGN KEY("ingredient11_id") REFERENCES "Ingredients",
 	FOREIGN KEY("ingredient12_id") REFERENCES "Ingredients",
 	FOREIGN KEY("ingredient13_id") REFERENCES "Ingredients",
@@ -98,9 +118,28 @@ CREATE TABLE IF NOT EXISTS "Recipes_has_Ingredients" (
 	FOREIGN KEY("ingredient17_id") REFERENCES "Ingredients",
 	FOREIGN KEY("ingredient18_id") REFERENCES "Ingredients",
 	FOREIGN KEY("ingredient19_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient1_id") REFERENCES "Ingredients",
 	FOREIGN KEY("ingredient20_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient2_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient3_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient4_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient5_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient6_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient7_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient8_id") REFERENCES "Ingredients",
+	FOREIGN KEY("ingredient9_id") REFERENCES "Ingredients",
+	FOREIGN KEY("scales10_id") REFERENCES "Scales",
+	FOREIGN KEY("scales11_id") REFERENCES "Scales",
+	FOREIGN KEY("scales12_id") REFERENCES "Scales",
+	FOREIGN KEY("scales13_id") REFERENCES "Scales",
+	FOREIGN KEY("scales14_id") REFERENCES "Scales",
+	FOREIGN KEY("scales15_id") REFERENCES "Scales",
+	FOREIGN KEY("scales16_id") REFERENCES "Scales",
 	FOREIGN KEY("scales17_id") REFERENCES "Scales",
+	FOREIGN KEY("scales18_id") REFERENCES "Scales",
+	FOREIGN KEY("scales19_id") REFERENCES "Scales",
 	FOREIGN KEY("scales1_id") REFERENCES "Scales",
+	FOREIGN KEY("scales20_id") REFERENCES "Scales",
 	FOREIGN KEY("scales2_id") REFERENCES "Scales",
 	FOREIGN KEY("scales3_id") REFERENCES "Scales",
 	FOREIGN KEY("scales4_id") REFERENCES "Scales",
@@ -108,42 +147,20 @@ CREATE TABLE IF NOT EXISTS "Recipes_has_Ingredients" (
 	FOREIGN KEY("scales6_id") REFERENCES "Scales",
 	FOREIGN KEY("scales7_id") REFERENCES "Scales",
 	FOREIGN KEY("scales8_id") REFERENCES "Scales",
-	FOREIGN KEY("scales11_id") REFERENCES "Scales",
-	FOREIGN KEY("scales12_id") REFERENCES "Scales",
-	FOREIGN KEY("scales13_id") REFERENCES "Scales",
-	FOREIGN KEY("scales14_id") REFERENCES "Scales",
-	FOREIGN KEY("scales15_id") REFERENCES "Scales",
-	FOREIGN KEY("scales16_id") REFERENCES "Scales",
-	FOREIGN KEY("scales18_id") REFERENCES "Scales",
-	FOREIGN KEY("scales19_id") REFERENCES "Scales",
-	FOREIGN KEY("scales20_id") REFERENCES "Scales"
-);
-DROP TABLE IF EXISTS "Instructions";
-CREATE TABLE IF NOT EXISTS "Instructions" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"instruction"	TEXT NOT NULL
-);
-DROP TABLE IF EXISTS "Instructions_has_Recipes";
-CREATE TABLE IF NOT EXISTS "Instructions_has_Recipes" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"Recipes_id"	INTEGER NOT NULL,
-	"Instructions_id"	INTEGER NOT NULL,
-	"InstructionNb"	INTEGER NOT NULL,
-	FOREIGN KEY("Recipes_id") REFERENCES "Recipes",
-	FOREIGN KEY("Instructions_id") REFERENCES "Instructions"
-);
-DROP TABLE IF EXISTS "Recipes";
-CREATE TABLE IF NOT EXISTS "Recipes" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"title"	TEXT NOT NULL,
-	"completionTime"	INTEGER NOT NULL,
-	"lowBudget"	INTEGER NOT NULL,
-	"score"	INTEGER NOT NULL,
-	"imagePath"	TEXT NOT NULL
+	FOREIGN KEY("scales9_id") REFERENCES "Scales"
 );
 DROP TABLE IF EXISTS "Scales";
-CREATE TABLE IF NOT EXISTS "Scales" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"scaleName"	TEXT NOT NULL
+CREATE TABLE "Scales" (
+	"id"	INTEGER NOT NULL,
+	"scaleName_en"	TEXT,
+	"scaleName_fr"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "TypesOfIngredient";
+CREATE TABLE "TypesOfIngredient" (
+	"id"	INTEGER,
+	"type_en"	TEXT,
+	"type_fr"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 COMMIT;
